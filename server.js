18e8ae597bad454c271d0e8e69b12c3a152fe774
alex-avila <alex.avilx@icloud.com>
2018-07-12 14:12:44 -0600

    make app ready for heroku deployment, hopefully

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const fileUpload = require('express-fileupload')
-const port = 8000
+const path = require('path')
+const port = process.env.PORT || 8000
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(fileUpload())
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-mongoose.connect('mongodb://localhost/full_stack_app', (err) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/full_stack_app', (err) => {
     if (err) throw err
     console.log('Connected to the database')
 })
 
 app.use('/decks', require('./routes/deckRoutes'))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
